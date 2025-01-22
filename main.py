871b965f43e93d9924e48ec80a4ceaca75867a58
Wyatt Johnson <116504461+realendie@users.noreply.github.com>
2025-01-22 15:10:53 -0500

    added introduction
    chromebook

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
+print('You awaken in a dimly lit chamber, the air thick with the scent of old parchment and burning candles.\nA robed figure stands before you, their face obscured by a hood.\n"Welcome, apprentice," the figure intones.\n"Your trial begins now. Prove yourself worthy of the title of wizard, or be cast into the void."')
 
+door = input('\nThe robed figure gestures to two doors.\nOne glows faintly blue, the other red.\n"Choose your path," they say.\n    1) Enter the blue door\n    2) Enter the red door')
